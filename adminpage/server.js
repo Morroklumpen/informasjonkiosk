f0a0eaa2a39b51fd9792ae293422b5181b5d98d3
Morroklumpen <tord.rustand@gmail.com>
2024-01-02 23:13:42 +0100

    rd
    sfsf

--- adminpage/server.js
+++ adminpage/server.js
@@ -1,36 +1,13 @@
-const express = require('express');
+const express = require('express')
+const app = express()
 const path = require('path');
-const app = express();
- 
-// Set public folder as static
-app.use(express.static(path.join(__dirname)));
- 
-// Custom route handling
-app.get('/custom-route', (req, res, next) => {
-    // Simulate a 404 error for demonstration purposes
-    const err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
- 
-// Error handling middleware
-app.use((err, req, res, next) => {
-  if (err) {
-      console.error(err.stack);
-      if (err.status === 404) {
-          res.status(404).sendFile(path.join(__dirname, '404', '404.html'));
-      }
-  } else {
-      next();
-  }
-});
- 
-// Not found in static files, so default to index.html
-app.use((req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
- 
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
+const port = 3000
 
+app.get('/', (req, res) => {
+  // res.send('Hello World!')
+    res.sendFile(path.join(__dirname,'./login.html'))
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
